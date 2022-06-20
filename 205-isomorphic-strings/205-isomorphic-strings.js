10220c39ae95d836bc14ce11812bb05d5ba00ce7
Hossam El-Adawy <36332545+Hossam4M@users.noreply.github.com>
2022-06-20 18:53:13 +0200

    Time: 163 ms (6.92%), Space: 47.4 MB (13.66%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -8,16 +8,15 @@ var isIsomorphic = function(s, t) {
     const mapTtoS = {};
     
     for (const i in s) {
+        if (mapTtoS[t[i]] && mapTtoS[t[i]] !== s[i]) return false;
+        if (mapStoT[s[i]] && mapStoT[s[i]] !== t[i]) return false;
+        
         if (!mapStoT[s[i]]) {
-            if (mapTtoS[t[i]] && mapTtoS[t[i]] !== s[i]) return false
             mapStoT[s[i]] = t[i];
-            if (!mapTtoS[t[i]]) { mapTtoS[t[i]] = s[i]}
+            mapTtoS[t[i]] = s[i];
             continue;
         };
         
-        if (mapStoT[s[i]] === t[i] && mapTtoS[t[i]] === s[i]) continue;
-        
-        return false
     }
     
     return true
