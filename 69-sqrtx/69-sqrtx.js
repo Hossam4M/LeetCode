b2e65edc2645e8ff6c58e1631012312dd34a5add
Hossam El-Adawy <36332545+Hossam4M@users.noreply.github.com>
2022-06-21 15:04:31 +0200

    Time: 252 ms (5.00%), Space: 44.1 MB (23.94%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -5,7 +5,8 @@
 var mySqrt = function(x) {
     if (x === 0 || x === 1) return x;
     if (x === 2) return 1;
-    for (let i = 1; i < x; i++) {
+    
+    for (let i = 1; i <= Math.ceil(x / 2); i++) {
         if (Math.floor((x / i) - i) === 0) return i;
         if (Math.floor((x / i) - i) === -1) return i - 1;
     }
