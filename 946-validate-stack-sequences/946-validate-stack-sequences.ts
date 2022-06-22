5d4790c841904a753a70b5c368bcf07e1d640343
Hossam El-Adawy <36332545+Hossam4M@users.noreply.github.com>
2022-06-22 19:06:37 +0200

    Time: 144 ms (10.00%), Space: 45 MB (100.00%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.ts
@@ -0,0 +1,15 @@
+function validateStackSequences(pushed: number[], popped: number[]): boolean {
+    
+    const stack = []
+    let i = 0
+
+    for (const el of pushed) {
+        stack.push(el)
+        while (stack.length && stack[stack.length - 1] === popped[i]) {
+            stack.pop()
+            i++
+        }
+    }
+    
+    return pushed.length === i
+};
