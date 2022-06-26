11a37f2542223951b45887a0461167b4a9254e23
Hossam El-Adawy <36332545+Hossam4M@users.noreply.github.com>
2022-06-26 15:05:12 +0200

    Time: 135 ms (17.21%), Space: 43.8 MB (62.89%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    if (!x) return 0
+    const sign = x / Math.abs(x);
+    const result = sign * Number(String(Math.abs(x)).split('').reverse().join(''))
+    
+    return Math.abs(result) >= Math.pow(2, 31) ? 0 : result
+};
