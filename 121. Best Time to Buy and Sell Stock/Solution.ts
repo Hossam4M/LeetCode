65187e99b785b3f7eecf7c1a1d1db311a515eaf8
Hossam El-Adawy <36332545+Hossam4M@users.noreply.github.com>
2024-12-28 23:33:33 +0200

    TypeScript Solution
            Submission Statistics:
            Question #: 121
            Language: TypeScript
            Runtime: 16 ms
            Runtime percentile: 5.37
            Memory: 67.896 MB
            Memory percentile: 5.05

---
+++ 121. Best Time to Buy and Sell Stock/Solution.ts
@@ -0,0 +1,27 @@
+function maxProfit(prices: number[]): number {
+    let buy = -1;
+    let sell = -1;
+
+    let result = -1
+
+    for (const [index, price] of prices.entries()) {
+        if (index === 0) {
+            buy = price
+            sell = price
+            result = 0
+        } else {
+            if (sell < price) {
+                sell = price;
+                result = Math.max(0, sell - buy, result)
+            }
+
+            if (buy > price) {
+                buy = price;
+                sell = price;
+                result = Math.max(0, sell - buy, result)
+            }
+        }
+    }
+
+    return result
+};
